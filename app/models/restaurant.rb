41ea88f2fd9b55f9ddf36b9b15dc714ee885f6d2
Sina Bengsch <sinabengsch@gmail.com>
2020-05-20 14:37:06 +0200

    created and migrated schema, added data via seed file, added models, validations, controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,10 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  def self.category_array
+    ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  end
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: category_array }
 end
